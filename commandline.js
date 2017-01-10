fbdb42ce5b5651b87e2193177297ac722fe1fc44
Ogunsola damilola <damilola.olapeju@gmail.com>
2017-01-10 17:35:34 +0100

    command line task

--- commandline.js
+++ commandline.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 
 
-axios.get('https://api.github.com/users/sdkcodes')
+axios.get('https://api.github.com/users/Olapeju')
   .then(function (response) {
     console.log(response);
   })
